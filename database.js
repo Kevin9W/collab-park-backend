edcd75e6997b737a26b46cac8134cef90e9f0096
Kevin <kevin9wong@gmail.com>
2019-12-18 12:50:02 -0800

    lets backend connect to frontend

--- database.js
+++ database.js
@@ -1,9 +1,11 @@
 const sqlite3 = require('sqlite3')
 const database = new sqlite3.Database('./database.db')
 //---Creating Table Queries---
-const createEmailPrefQuery = "CREATE TABLE IF NOT EXIST email_pref (first_name TEXT, last_name TEXT, email TEXT, email_all BOOLEAN"
+const createEmailPrefQuery = "CREATE TABLE IF NOT EXISTS email_pref (first_name TEXT, last_name TEXT, email TEXT, events BOOLEAN, discount BOOLEAN, news_updates BOOLEAN, frequency TEXT)"
 //---Creates Tables---
 database.run(createEmailPrefQuery, error=>{
-	if (error) console.log(new Error("Create email pref table failed"),error)
+	if (error) console.log(new Error("Create email_pref table failed"),error)
 	else console.log("Create email pref table succeded")
 })
+
+module.exports=database
--- server.js
+++ server.js
@@ -6,14 +6,20 @@ const PORT=3000
 
 app.use(express.json())
 
+app.use(function(req, res, next) {
+ 	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.get('/',(request,response)=>{
 	response.send("o3o")
 })
 
-app.post('/signup',(request,response)=>{
-	let createNewEmail ='INSERT INTO email_pref VALUES(?,?,?,?)'
+app.post('/api/signup',(request,response)=>{
+	let createNewEmail ='INSERT INTO email_pref VALUES(?,?,?,?,?,?,?)'
 	let reqBody=Object.values(request.body)
-	database.run(createNewEmail,reqBody,(error,result)=>{
+	database.run(createNewEmail,reqBody, error=>{
 		if(error){
 			console.log("Failed to make new email",error);
 			response.sendStatus(500)
